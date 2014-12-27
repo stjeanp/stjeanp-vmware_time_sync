a7f3fef5b599d312f206214a190794013ba30e7d
Patrick St. Jean <stjeanp@pat-st-jean.com>
2014-12-27 13:55:27 -0600

    Update rspec tests for readability

--- spec/classes/vmware_time_sync_spec.rb
+++ spec/classes/vmware_time_sync_spec.rb
@@ -1,10 +1,23 @@
-require 'rspec-puppet'
 require 'spec_helper'
 
 describe 'vmware_time_sync', :type => :class do
+  after :each do
+    Facter.clear
+    Facter.clear_messages
+  end
+
   context 'Enable on something not a VMware VM' do
-    let(:params) { {:enable_sync => true} }
-    let(:facts) { {:virtual => '', :vmware_tools_timesync => 'disabled'} }
+    let :params do
+      {
+        :enable_sync => true,
+      }
+    end
+    let :facts do
+      {
+        :virtual => '',
+        :vmware_tools_timesync => 'disabled',
+      }
+    end
 
     it do
       expect {
@@ -14,8 +27,17 @@ describe 'vmware_time_sync', :type => :class do
   end
 
   context 'Disable on something not a VMware VM' do
-    let(:params) { {:enable_sync => false} }
-    let(:facts) { {:virtual => '', :vmware_tools_timesync => 'disabled'} }
+    let :params do
+      {
+        :enable_sync => false,
+      }
+    end
+    let :facts do
+      {
+        :virtual => '',
+        :vmware_tools_timesync => 'enabled',
+      }
+    end
 
     it do
       expect {
@@ -25,8 +47,16 @@ describe 'vmware_time_sync', :type => :class do
   end
 
   context 'Handle missing custom fact' do
-    let(:params) { {:enable_sync => false} }
-    let(:facts) { {:virtual => 'vmware'} }
+    let :params do
+      {
+        :enable_sync => false,
+      }
+    end
+    let :facts do
+      {
+        :virtual => 'vmware',
+      }
+    end
 
     it do
       expect {
@@ -36,8 +66,17 @@ describe 'vmware_time_sync', :type => :class do
   end
 
   context 'Handle bad custom fact' do
-    let(:params) { {:enable_sync => false} }
-    let(:facts) { {:virtual => 'vmware', :vmware_tools_timesync => 'unknown'} }
+    let :params do
+      {
+        :enable_sync => false,
+      }
+    end
+    let :facts do
+      {
+        :virtual => 'vmware',
+        :vmware_tools_timesync => 'unknown',
+      }
+    end
 
     it do
       expect {
@@ -47,16 +86,34 @@ describe 'vmware_time_sync', :type => :class do
   end
 
   context 'Disable on a VMware VM that is already disabled' do
-    let(:params) { {:enable_sync => false} }
-    let(:facts) { {:virtual => 'vmware', :vmware_tools_timesync => 'disabled'} }
+    let :params do
+      {
+        :enable_sync => false,
+      }
+    end
+    let :facts do
+      {
+        :virtual => 'vmware',
+        :vmware_tools_timesync => 'disabled',
+      }
+    end
 
     it { should compile }
     it { should_not contain_exec('timesync') }
   end
 
   context 'Disable on a VMware VM that is enabled' do
-    let(:params) { {:enable_sync => false} }
-    let(:facts) { {:virtual => 'vmware', :vmware_tools_timesync => 'enabled'} }
+    let :params do
+      {
+        :enable_sync => false,
+      }
+    end
+    let :facts do
+      {
+        :virtual => 'vmware',
+        :vmware_tools_timesync => 'enabled',
+      }
+    end
 
     it { should compile }
     it { should contain_exec('timesync').with(
@@ -65,16 +122,34 @@ describe 'vmware_time_sync', :type => :class do
   end
 
   context 'Enable on a VMware VM that is enabled' do
-    let(:params) { {:enable_sync => true} }
-    let(:facts) { {:virtual => 'vmware', :vmware_tools_timesync => 'enabled'} }
+    let :params do
+      {
+        :enable_sync => true,
+      }
+    end
+    let :facts do
+      {
+        :virtual => 'vmware',
+        :vmware_tools_timesync => 'enabled',
+      }
+    end
 
     it { should compile }
     it { should_not contain_exec('timesync') }
   end
 
   context 'Enable on a VMware VM that is disabled' do
-    let(:params) { {:enable_sync => true} }
-    let(:facts) { {:virtual => 'vmware', :vmware_tools_timesync => 'disabled'} }
+    let :params do
+      {
+        :enable_sync => true,
+      }
+    end
+    let :facts do
+      {
+        :virtual => 'vmware',
+        :vmware_tools_timesync => 'disabled',
+      }
+    end
 
     it { should compile }
     it { should contain_exec('timesync').with(
